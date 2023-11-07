b9d5b0495de719c02791d8745807642a7227ceae
Erick Toala <toalaerick56@gmail.com>
2023-11-07 13:05:07 -0500

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.blue;
+
+const fromTextStyle = TextStyle(
+  color: Color.fromARGB(255, 255, 255, 255),
+  fontSize: 15.0,
+  fontWeight: FontWeight.w700,
+);
+
+const dateTextStyle = TextStyle(
+  color: Color.fromARGB(115, 255, 255, 255),
+  fontSize: 14.0,
+  fontWeight: FontWeight.w300,
+);
+
+const subjectTextStyle = TextStyle(
+  color: Color.fromARGB(255, 255, 255, 255),
+  fontSize: 15.0,
+  fontWeight: FontWeight.w400,
+);
+
+const bodyTextStyle = TextStyle(
+  color: Color.fromARGB(255, 255, 255, 255),
+  fontSize: 15.0,
+);
